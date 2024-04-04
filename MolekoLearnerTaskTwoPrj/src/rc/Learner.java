fed70613ccd4d3d0841c65c1899ee555b01908e5
king-maverickk-school <st10461408@rcconnect.edu.za>
2024-04-04 08:35:32 +0000

    refactor: declare variables and initialise them under default constructor

--- MolekoLearnerTaskTwoPrj/src/rc/Learner.java
+++ MolekoLearnerTaskTwoPrj/src/rc/Learner.java
@@ -5,5 +5,18 @@ package rc;
  * @author Motheo Moleko [ST10461408]
  */
 public class Learner {
+    // Attributes [ full name, subject, Assignment Mark, Test Mark, Exam Mark]
+    // declaration of variables/attributes
+    String fullName, subject;
+    double assignMark, testMark, examMark;
+    
+    // Default Constructor for Learner
+    public Learner(){
+        this.fullName = "";
+        this.subject = "";
+        this.assignMark = 0.0;
+        this.testMark = 0.0;
+        this.examMark = 0.0;
+    }
     
 }
